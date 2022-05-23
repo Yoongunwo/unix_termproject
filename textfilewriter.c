1b21821547e63160c8f2aac11bc3fb8af42db914
yhlee <pay2889@gmail.com>
2022-05-23 09:15:54 +0900

    yhlee
    edit read_file() function.

--- textfilewriter.c
+++ textfilewriter.c
@@ -6,11 +6,8 @@ void create_music_titles(FILE* stream) {
 	return;
 }
 
-void read_file(char* file_name) {
-	return;
-}
-
 void write_file(char* file_name) {
+	
 	return;
 }
 
@@ -18,33 +15,33 @@ void write_file(char* file_name) {
 void read_file(char* file_name) {
 	FILE* fp = fopen(file_name, "r");
 
-	if (fp == NULL) {
-		printf("Can't Read File!\n");
+	if(fp == NULL){
+		printf("Can't Find File!\n");
 		return;
 	}
 
-	else {
-		// 첫 줄에는 곡의 개수가 입력된다.
-		int song_num;
-		char* arr[MAX_TITLE_SIZE] = NULL;
-		fscanf(fp, "%d\n", &song_num);
+	else{
+		int num_of_song;
+		int max = 100;
+		char line[max];
+		char *pLine;
+		int counter = 1;
+		fscanf(fp, "%d", &num_of_song);
 
-		for (int i = 0; i < song_num; i++) {
-			fscanf(fp, "%s\n", arr[i]);
+		printf("Total number of songs is %d\n", num_of_song);
+		pLine = fgets(line, max, fp); // delete trash value
+		while(!feof(fp) && (counter <= num_of_song)){
+			pLine = fgets(line, max, fp);
+			printf("%d: %s", counter,pLine);
+			counter++;
 		}
-
-		// test
-
-		for (int j = 0; j < song_num; j++) {
-			printf("%s\n", arr[j]);
-		}
+		fclose(fp);
 	}
 
 }
 int main() {
-	char* file_name = "Text.txt";
+	char* file_name = "./Text.txt";
 
 	read_file(file_name);
+	read_file("sexy.txt");
 }
-
-// 현재 컴파일이 안된다 왜이럼..?
