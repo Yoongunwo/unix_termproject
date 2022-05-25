847b31d1d00ecfa97d788f0aa438f16df393aa77
yhlee <pay2889@gmail.com>
2022-05-25 10:04:26 +0900

    yhlee
    modify read_file() <- contain "control the command line" func

--- textfilewriter.c
+++ textfilewriter.c
@@ -3,6 +3,10 @@
 
 #define MAX_TITLE_SIZE 50 
 // We assume TOTAL_NUM_OF_MUSIC is less than 50
+//
+// "print_file" will be used in read_file.
+void print_file(FILE* stream);
+
 
 void create_music_titles(FILE* stream) {
 	return;
@@ -49,6 +53,8 @@ void read_file(char* file_name) {
 		int num_of_song = -1;
 		char* title_of_song[MAX_TITLE_SIZE] = {0};
 		fscanf(fp, "%d\n", &num_of_song);
+
+		// If file is empty
 		if(num_of_song == -1){
 			printf("Empty File!\n");
 			return;
@@ -59,15 +65,35 @@ void read_file(char* file_name) {
 			fgets(title_of_song[i], MAX_TITLE_SIZE, fp); 
 		}
 
+		// if I have to control the command(player),
+		int command_num = -1;
+		char* command[MAX_TITLE_SIZE] = {0};
+		fscanf(fp, "%d\n", &command_num);
+
+		if(command_num == -1){
+			printf("command is empty!\n");
+		}
+		
+		for(int i=0; i<command_num; i++){
+			*(command + i) = (char*)malloc(sizeof(char*));
+			fgets(command[i], MAX_TITLE_SIZE, fp);
+		}
+
 		for (int i=0; i<num_of_song; i++){
 			printf("%s", title_of_song[i]);
 			free(*(title_of_song+i));
 		}
 
+		for (int i=0; i<command_num; i++){
+			printf("%s", command[i]);
+			free(*(command + i));
+		}
+
+		
 		fclose(fp);
 		return;
+
 	}
-	return;
 
 }
 int main() {
